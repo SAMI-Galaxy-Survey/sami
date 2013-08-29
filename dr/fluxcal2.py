93ea866752cb187193952c0188c53c24e6bffae1
James T. Allen <j.allen@physics.usyd.edu.au>
2013-08-29 15:50:39 +1000

    Some PSF information saved to header.

--- dr/fluxcal2.py
+++ dr/fluxcal2.py
@@ -434,7 +434,7 @@ def derive_transfer_function(path_list, max_sep_arcsec=30.0,
         observed_flux, observed_background = extract_total_flux(
             ifu, psf_parameters, model_name)
         save_extracted_flux(path, observed_flux, observed_background,
-                            star_match)
+                            star_match, psf_parameters, model_name)
         transfer_function = take_ratio(
             standard_data['flux'], 
             standard_data['wavelength'], 
@@ -531,7 +531,7 @@ def residual_slice(flux_background, model, data, variance):
     #return ((background + flux * model) - data) / np.sqrt(variance)
 
 def save_extracted_flux(path, observed_flux, observed_background,
-                        star_match):
+                        star_match, psf_parameters, model_name):
     """Add the extracted flux to the specified FITS file."""
     # Turn the data into a single array
     data = np.vstack((observed_flux, observed_background))
@@ -546,9 +546,17 @@ def save_extracted_flux(path, observed_flux, observed_background,
         ('STDFILE', star_match['path'], 'Filename of standard spectrum'),
         ('STDOFF', star_match['separation'], 'Offset (arcsec) to standard '
                                              'star coordinates'),
-        ('HGFLXCAL', HG_CHANGESET, 'Hg changeset ID for fluxcal code')]
+        ('HGFLXCAL', HG_CHANGESET, 'Hg changeset ID for fluxcal code'),
+        ('MODEL', model_name, 'Name of model used in PSF fit')]
+    for key, value in psf_parameters.items():
+        header_item_list.append((header_translate(key), value, 
+                                 'PSF model parameter'))
     for key, value, comment in header_item_list:
-        new_hdu.header[key] = (value, comment)
+        try:
+            new_hdu.header[key] = (value, comment)
+        except ValueError:
+            # Probably tried to save an array, just ditch it
+            pass
     # Update the file
     hdulist = pf.open(path, 'update', do_not_scale_image_data=True)
     # Check if there's already an extracted flux, and delete if so
@@ -562,6 +570,22 @@ def save_extracted_flux(path, observed_flux, observed_background,
     hdulist.close()
     del hdulist
     return
+
+def header_translate(key):
+    """Translate parameter names to be suitable for headers."""
+    name_dict = {'xcen_ref': 'XCENREF',
+                 'ycen_ref': 'YCENREF',
+                 'zenith_direction': 'ZENDIR',
+                 'zenith_distance': 'ZENDIST',
+                 'flux': 'FLUX',
+                 'beta': 'BETA',
+                 'background': 'BCKGRND',
+                 'alpha_ref': 'ALPHAREF'}
+    try:
+        header_name = name_dict[key]
+    except KeyError:
+        header_name = key[:8]
+    return header_name
 
 def save_transfer_function(path, transfer_function):
     """Add the transfer function to a pre-existing FLUX_CALIBRATION HDU."""
