02e3163c7bc17604fbb543819fbfc1f078d76732
James T. Allen <j.allen@physics.usyd.edu.au>
2014-01-03 10:32:43 +1100

    Moved temp_imp_scratch from manager.py to tdfdr.py

--- dr/tdfdr.py
+++ dr/tdfdr.py
@@ -1,5 +1,7 @@
 import subprocess
 import os
+import tempfile
+import shutil
 from contextlib import contextmanager
 
 def run_2dfdr_single(fits, idx_file, options=None, cwd=None):
@@ -49,3 +51,38 @@ def visit_dir(dir_path, return_to=None, cleanup_2dfdr=False):
         if cleanup_2dfdr:
             cleanup()
         os.chdir(return_to)
+
+@contextmanager
+def temp_imp_scratch(restore_to=None, scratch_dir=None, do_not_delete=False):
+    """
+    Create a temporary directory for 2dfdr IMP_SCRATCH,
+    allowing multiple instances of 2dfdr to be run simultaneously.
+    """
+    # Use current value for restore_to if not provided
+    if restore_to is None:
+        restore_to = os.environ['IMP_SCRATCH']
+    # Make the parent directory, if specified
+    # If not specified, tempfile.mkdtemp will choose a suitable location
+    if scratch_dir is not None and not os.path.exists(scratch_dir):
+        os.makedirs(scratch_dir)
+    # Make a temporary directory with a unique name
+    imp_scratch = tempfile.mkdtemp(dir=scratch_dir)
+    # Set the IMP_SCRATCH environment variable to that directory, so that
+    # 2dfdr will use it
+    os.environ['IMP_SCRATCH'] = imp_scratch
+    try:
+        yield
+    finally:
+        # Change the IMP_SCRATCH environment variable back to what it was
+        os.environ['IMP_SCRATCH'] = restore_to
+        if not do_not_delete:
+            # Remove the temporary directory and all its contents
+            shutil.rmtree(imp_scratch)
+            # Remove any parent directories that are empty
+            try:
+                os.removedirs(os.path.dirname(imp_scratch))
+            except OSError:
+                # It wasn't empty; never mind
+                pass
+    return
+
