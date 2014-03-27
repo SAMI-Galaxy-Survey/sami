6a5f32d9dbce9ed92ecc3927031fe26a687a6800
James T. Allen <j.allen@physics.usyd.edu.au>
2014-03-27 13:31:39 +1100

    Combine red/blue data for stability calculations.

--- qc/fluxcal.py
+++ qc/fluxcal.py
@@ -37,44 +37,84 @@ def stability(mngr):
     Return arrays of flux calibration stability, defined as the standard
     deviation of the end-to-end throughput as a function of wavelength.
 
-    Two dictionaries are returned, one for each CCD. Each dictionary
-    contains 'wavelength', 'std_combined', 'std_individual', 
-    'mean_individual' and 'mean_combined'.
+    The return value is a dictionary containing arrays of 'wavelength', 
+    'std_combined', 'std_individual', 'mean_individual' and 'mean_combined'.
+    The input 'data_combined' and 'data_individual' are included.
+    Finally, the mean and standard deviation of the normalisation,
+    'mean_norm_comined', 'mean_norm_individual', 'std_norm_combined' and
+    'std_norm_individual', are included.
     """
-    result = []
-    for files_dict in fluxcal_files(mngr):
-        result_ccd = {}
-        n_pix = pf.getval(files_dict.keys()[0], 'NAXIS1')
-        n_combined = len(files_dict)
-        n_individual = sum(len(l) for l in files_dict.values())
-        data_combined = np.zeros((n_combined, n_pix))
-        data_individual = np.zeros((n_individual, n_pix))
-        i_individual = 0
-        for i_combined, path_combined in enumerate(files_dict.keys()):
-            hdulist = pf.open(path_combined)
-            data_combined[i_combined, :] = 1.0 / hdulist[0].data
-            for hdu in hdulist[1:]:
-                data_individual[i_individual, :] = 1.0 / hdu.data[2, :]
-                i_individual += 1
-        norm_combined = np.median(data_combined, axis=1)
-        norm_individual = np.median(data_individual, axis=1)
-        data_norm_combined = (data_combined.T / norm_combined).T
-        data_norm_individual = (data_individual.T / norm_individual).T
-        result_ccd['std_combined'] = np.std(data_norm_combined, axis=0)
-        result_ccd['std_individual'] = np.std(data_norm_individual, axis=0)
-        result_ccd['mean_combined'] = np.mean(data_norm_combined, axis=0)
-        result_ccd['mean_individual'] = np.mean(data_norm_individual, axis=0)
-        result_ccd['std_norm_combined'] = np.std(norm_combined)
-        result_ccd['std_norm_individual'] = np.std(norm_individual)
-        result_ccd['mean_norm_combined'] = np.mean(norm_combined)
-        result_ccd['mean_norm_individual'] = np.mean(norm_individual)
-        header = hdulist[0].header
+    result = {}
+    file_pairs = []
+    individual_file_pairs = []
+    file_dicts = fluxcal_files(mngr)
+    n_individual = 0
+    for path_1 in file_dicts[0]:
+        path_2 = path_1.replace('ccd_1', 'ccd_2')
+        if path_2 in file_dicts[1]:
+            file_pairs.append((path_1, path_2))
+            filename_1_list = [
+                os.path.basename(f) for f in file_dicts[0][path_1]]
+            filename_2_list = [
+                os.path.basename(f) for f in file_dicts[1][path_2]]
+            for i_filename_1, filename_1 in enumerate(filename_1_list):
+                filename_2 = filename_1[:5] + '2' + filename_1[6:]
+                if filename_2 in filename_2_list:
+                    i_filename_2 = filename_2_list.index(filename_2)
+                    individual_file_pairs.append((
+                        file_dicts[0][path_1][i_filename_1],
+                        file_dicts[1][path_2][i_filename_2]))
+    n_pix_1 = pf.getval(file_pairs[0][0], 'NAXIS1')
+    n_pix_2 = pf.getval(file_pairs[0][1], 'NAXIS1')
+    n_combined = len(file_pairs)
+    n_individual = len(individual_file_pairs)
+    data_combined = np.zeros((n_pix_1+n_pix_2, n_combined))
+    data_individual = np.zeros((n_pix_1+n_pix_2, n_individual))
+    i_individual = 0
+    for i_combined, (path_1, path_2) in enumerate(file_pairs):
+        data_combined[:n_pix_1, i_combined] = 1.0 / pf.getdata(path_1)
+        data_combined[n_pix_1:, i_combined] = 1.0 / pf.getdata(path_2)
+    for i_individual, (path_1, path_2) in enumerate(individual_file_pairs):
+        data_individual[:n_pix_1, i_individual] = 1.0 / pf.getdata(
+            path_1, 'FLUX_CALIBRATION')[2, :]
+        data_individual[n_pix_1:, i_individual] = 1.0 / pf.getdata(
+            path_2, 'FLUX_CALIBRATION')[2, :]
+    common = (
+        np.sum(np.isfinite(data_combined), axis=1) + 
+        np.sum(np.isfinite(data_individual), axis=1)
+        ) == (n_combined + n_individual)
+    norm_combined = np.mean(
+        data_combined[common, :], axis=0)
+    norm_individual = np.mean(
+        data_individual[common, :], axis=0)
+    data_norm_combined = data_combined / norm_combined
+    data_norm_individual = data_individual / norm_individual
+    result['std_combined'] = np.std(data_norm_combined, axis=1)
+    result['std_individual'] = np.std(data_norm_individual, axis=1)
+    result['mean_combined'] = np.mean(data_norm_combined, axis=1)
+    result['mean_individual'] = np.mean(data_norm_individual, axis=1)
+    result['std_norm_combined'] = np.std(norm_combined)
+    result['std_norm_individual'] = np.std(norm_individual)
+    result['mean_norm_combined'] = np.mean(norm_combined)
+    result['mean_norm_individual'] = np.mean(norm_individual)
+    result['data_combined'] = data_combined
+    result['data_individual'] = data_individual
+    wavelength = np.zeros(n_pix_1 + n_pix_2)
+    for i, path in enumerate(file_pairs[0]):
+        header = pf.getheader(path)
         crval1 = header['CRVAL1']
         cdelt1 = header['CDELT1']
         crpix1 = header['CRPIX1']
-        result_ccd['wavelength'] = crval1 + cdelt1 * (
+        if i == 0:
+            start = 0
+            finish = n_pix_1
+            n_pix = n_pix_1
+        else:
+            start = n_pix_1
+            finish = n_pix_1 + n_pix_2
+            n_pix = n_pix_2
+        wavelength[start:finish] = crval1 + cdelt1 * (
             np.arange(n_pix) + 1 - crpix1)
-        result_ccd['data_combined'] = data_combined
-        result_ccd['data_individual'] = data_individual
-        result.append(result_ccd)
-    return tuple(result)
+    result['wavelength'] = wavelength
+    return result
+
