a99986ccbbd7eed6398355a11483b8c677f187e5
James T. Allen <j.allen@physics.usyd.edu.au>
2014-04-07 14:24:13 +1000

    QC function to measure S/N in telluric correction.

--- qc/telluric.py
+++ qc/telluric.py
@@ -14,3 +14,37 @@ def compare_star_field(path_pair, output=None):
     if output is not None:
         pf.PrimaryHDU(result).writeto(output)
     return result
+
+def snr_in_all_tellurics(mngr):
+    """Return array of S/N in telluric region in star in each frame."""
+    telluric_limits = [[6850, 6960],
+                       [7130, 7360],
+                       [7560, 7770],
+                       [8100, 8360]]
+    result = []
+    for fits in mngr.files(ndf_class='MFOBJECT', telluric_corrected=True,
+                           min_exposure=900.0, ccd='ccd_2'):
+        telluric_data = pf.getdata(fits.telluric_path, 'FLUX_CALIBRATION')
+        header = pf.getheader(fits.telluric_path)
+        wavelength = header['CRVAL1'] + header['CDELT1'] * (
+            1 + np.arange(header['NAXIS1']) - header['CRPIX1'])
+        in_telluric = np.zeros(header['NAXIS1'], dtype=bool)
+        for telluric_limits_single in telluric_limits:
+            in_telluric[(wavelength >= telluric_limits_single[0]) &
+                        (wavelength <= telluric_limits_single[1])] = True
+            # If there are only a few non-telluric pixels at the end of the
+            # spectrum, mark them as telluric anyway, in case the primary flux
+            # calibration screwed them up.
+            minimum_end_pixels = 50
+            n_blue_end = np.sum(wavelength < telluric_limits_single[0])
+            if n_blue_end > 0 and n_blue_end < minimum_end_pixels:
+                in_telluric[wavelength < telluric_limits_single[0]] = True
+            n_red_end = np.sum(wavelength > telluric_limits_single[1])
+            if n_red_end > 0 and n_red_end < minimum_end_pixels:
+                in_telluric[wavelength > telluric_limits_single[1]] = True
+            in_telluric[~(np.isfinite(telluric_data[0, :]) & 
+                          np.isfinite(telluric_data[2, :]))] = False
+        result.append(np.median(telluric_data[5, in_telluric] / 
+                                telluric_data[6, in_telluric]))
+    return np.array(result)
+    
