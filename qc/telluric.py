4d67d6e9990ef4aaa78bbd2f90dcb47013997eb8
James T. Allen <j.allen@physics.usyd.edu.au>
2014-04-08 09:56:43 +1000

    Update to previous QC telluric check to match new data.

--- qc/telluric.py
+++ qc/telluric.py
@@ -7,7 +7,7 @@ def compare_star_field(path_pair, output=None):
     """Compare measurements between all stars in a field."""
     n_hexa = 13
     n_pixel = pf.getval(path_pair[1], 'NAXIS1')
-    result = np.zeros((n_hexa, 4, n_pixel))
+    result = np.zeros((n_hexa, 7, n_pixel))
     for i_hexa in xrange(n_hexa):
         telluric.derive_transfer_function(path_pair, use_probe=i_hexa+1)
         result[i_hexa, :, :] = pf.getdata(path_pair[1], 'FLUX_CALIBRATION')
