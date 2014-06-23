efe06f9b8b77ce4ed68460b4114d83089ebbce28
James T. Allen <j.allen@physics.usyd.edu.au>
2014-06-23 12:44:51 +0100

    Combine throughputs with median; by default always use new values.

--- dr/throughput.py
+++ dr/throughput.py
@@ -24,30 +24,47 @@ def edit_throughput(hdulist, fibres, new_throughputs):
     hdu.data[fibres] = new_throughputs
     return
 
-def make_thput_file(path, throughput):
+def make_thput_file(path, new_thput, old_thput, used, path_list):
     """Makes a file containing a set of throughput values, for use by 2dfdr."""
-    hdu = pf.ImageHDU(throughput, name='THPUT')
     primary_hdu = pf.PrimaryHDU()
-    hdulist = pf.HDUList([primary_hdu, hdu])
+    hdu = pf.ImageHDU(new_thput, name='THPUT')
+    hdulist = [primary_hdu, hdu]
+    for idx, (old_thput_i, used_i, path_i) in enumerate(zip(
+            old_thput, used, path_list)):
+        data = np.vstack((old_thput_i, used_i))
+        hdu_i = pf.ImageHDU(data, name='INPUT')
+        hdu_i.header['EXTVER'] = idx + 1
+        hdu_i.header['INPATH'] = path_i
+        hdulist.append(hdu_i)
+    hdulist = pf.HDUList(hdulist)
     hdulist.writeto(path)
     return
 
-def make_clipped_thput_files(path_list, overwrite=True):
-    """Make thput files with bad values replaced by a mean."""
+def make_clipped_thput_files(path_list, overwrite=True, edit_all=False,
+                             median=False):
+    """Make thput files with bad values replaced by an average."""
     n_file = len(path_list)
     n_fibre = pf.getval(path_list[0], 'NAXIS1', 'THPUT')
     thput = np.zeros((n_file, n_fibre))
     for index in xrange(n_file):
         thput[index, :] = pf.getdata(path_list[index], 'THPUT')
     new_thput = np.zeros((n_file, n_fibre))
-    # 2dfdr now replaces dodgy throughput values with 0, which helps
-    good = (thput > 0)
-    new_thput[good] = thput[good]
-    mean_thput = np.sum(thput * good, 0) / np.sum(good, 0)
-    mean_thput = np.outer(np.ones(n_file), mean_thput)
-    new_thput[~good] = mean_thput[~good]
-    # Check which of the files have had their values edited
-    edited = (np.sum(good, 1) != n_fibre)
+    # 2dfdr Gauss extraction now replaces dodgy throughput values with 0
+    good = (thput > 0) & np.isfinite(thput)
+    if median:
+        avg_thput = np.array([np.median(thput[good[:, i], i])
+                             for i in xrange(n_fibre)])
+    else:
+        avg_thput = np.sum(thput * good, 0) / np.sum(good, 0)
+    avg_thput = np.outer(np.ones(n_file), avg_thput)
+    if edit_all:
+        new_thput = avg_thput
+        edited = np.ones(n_file, bool)
+    else:
+        new_thput[good] = thput[good]
+        new_thput[~good] = avg_thput[~good]
+        # Check which of the files have had their values edited
+        edited = (np.sum(good, 1) != n_fibre)
     # Write new files where necessary
     for index in xrange(n_file):
         if edited[index]:
@@ -57,7 +74,8 @@ def make_clipped_thput_files(path_list, overwrite=True):
             if os.path.exists(path_out) and overwrite:
                 os.remove(path_out)
             if not os.path.exists(path_out):
-                make_thput_file(path_out, new_thput[index])
+                make_thput_file(path_out, new_thput[index], thput, good,
+                                path_list)
             else:
                 edited[index] = False
     return edited
