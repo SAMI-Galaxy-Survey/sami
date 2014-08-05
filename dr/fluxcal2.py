431187caf0402bc15737713788f7f5e3d1025698
Nic Scott <37605529+exet1530@users.noreply.github.com>
2014-08-05 11:37:00 +1000

    Added import_table and import_manytables to db/ingest
    NB Changes to dr/fluxcal2 and samifitting should not be propagated

--- dr/fluxcal2.py
+++ dr/fluxcal2.py
@@ -26,7 +26,7 @@ as free parameters too. Note, however, that the atmospheric parameters
 are completely degenerate with each other and with ZD.
 """
 
-import os
+import os,code
 
 import numpy as np
 from scipy.optimize import leastsq, curve_fit
