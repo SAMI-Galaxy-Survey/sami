08ce9114e548ab227bb9590481988dc48e437f89
James T. Allen <j.allen@physics.usyd.edu.au>
2014-08-28 13:15:13 +1000

    Careful about use of np.where, and close hdulist when done.

--- qc/covariance.py
+++ qc/covariance.py
@@ -55,11 +55,12 @@ def compare_variance(path):
     var_right = np.zeros(len(radii))
     var_wrong = np.zeros(len(radii))
     for i_rad, radius in enumerate(radii):
-        keep = np.where((x**2 + y**2) < radius**2)
-        x_keep, y_keep = keep
+        mask = (x**2 + y**2) < radius**2
+        x_keep, y_keep = np.where(mask)
         # var_right[i_rad] = np.median(sum_variance(var, covar, x_keep, y_keep))
-        var_right[i_rad] = np.median(bin_hdulist(hdulist, keep)[1])
+        var_right[i_rad] = np.median(bin_hdulist(hdulist, mask)[1])
         var_wrong[i_rad] = np.median(np.nansum(var[:, x_keep, y_keep], 1))
+    hdulist.close()
     return var_right, var_wrong
 
 
