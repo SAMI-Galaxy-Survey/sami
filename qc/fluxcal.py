4b35aaf39aa6890f32fea7daa01fb2735dc859cb
James T. Allen <j.allen@physics.usyd.edu.au>
2015-04-17 12:03:43 +1000

    Removed assumptions about spaxel size from qc/fluxcal.py

--- qc/fluxcal.py
+++ qc/fluxcal.py
@@ -688,7 +688,13 @@ def sum_spectrum_from_cube(file_pair, radius):
     """Return the summed spectrum from spaxels in the centre of a cube."""
     # Replace the hard-coded numbers with something smarter
     hdulist_pair = [pf.open(path) for path in file_pair]
-    x, y = np.meshgrid(0.5*(np.arange(50)-24.5), 0.5*(np.arange(50)-24.5))
+    n_pix = hdulist_pair[0][0].header['NAXIS1']
+    # Pixel scale in arcseconds
+    pix_scale = 3600.0 * np.abs(hdulist_pair[0][0].header['CDELT1'])
+    # Construct coordinates along a single axis
+    x_tmp = pix_scale * (np.arange(n_pix) - (n_pix - 1)/2.0)
+    # Expand that to a grid of coordinate values
+    x, y = np.meshgrid(x_tmp, x_tmp)
     weight_map = np.nansum(np.array((hdulist_pair[0]['WEIGHT'].data,
                                      hdulist_pair[1]['WEIGHT'].data)), (0, 1))
     cen_x = np.nansum(x * weight_map) / np.nansum(weight_map)
