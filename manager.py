24cbc55b96b8b407f1d62cb576d217866ae2c6b7
James T. Allen <j.allen@physics.usyd.edu.au>
2015-09-28 11:37:22 +1000

    Use grating (not CCD) to set idx files.

--- manager.py
+++ manager.py
@@ -114,14 +114,12 @@ else:
     def ICRS(*args, **kwargs):
         return coord.SkyCoord(*args, frame='icrs', **kwargs)
 
-IDX_FILES_SLOW = {'1': 'sami580V_v1_2.idx',
-                  '2': 'sami1000R_v1_2.idx',
-                  'ccd_1': 'sami580V_v1_2.idx',
-                  'ccd_2': 'sami1000R_v1_2.idx'}
-IDX_FILES_FAST = {'1': 'sami580V.idx',
-                  '2': 'sami1000R.idx',
-                  'ccd_1': 'sami580V.idx',
-                  'ccd_2': 'sami1000R.idx'}
+IDX_FILES_SLOW = {'580V': 'sami580V_v1_2.idx',
+                  '1500V': 'sami1500V_v1_2.idx',
+                  '1000R': 'sami1000R_v1_2.idx'}
+IDX_FILES_FAST = {'580V': 'sami580V.idx',
+                  '1500V': 'sami1500V.idx',
+                  '1000R': 'sami1000R.idx'}
 IDX_FILES = {'fast': IDX_FILES_FAST,
              'slow': IDX_FILES_SLOW}
 
@@ -1404,7 +1402,7 @@ class Manager:
         # First establish the 2dfdr options for all files that need reducing
         # Would be more memory-efficient to construct a generator
         input_list = [(fits, 
-                       self.idx_files[fits.ccd],
+                       self.idx_files[fits.grating],
                        tuple(self.tdfdr_options(
                            fits, throughput_method=throughput_method,
                            tlm=tlm)),
@@ -2238,7 +2236,7 @@ class Manager:
             return False
         options = self.tdfdr_options(fits, tlm=tlm)
         # All options have been set, so run 2dfdr
-        tdfdr.run_2dfdr_single(fits, self.idx_files[fits.ccd], 
+        tdfdr.run_2dfdr_single(fits, self.idx_files[fits.grating], 
                                options=options, cwd=self.cwd)
         if (fits.ndf_class == 'MFFFF' and tlm and not leave_reduced and
             os.path.exists(fits.reduced_path)):
@@ -2441,39 +2439,6 @@ class Manager:
                 options.extend(['-'+match_class.upper()+'_FILENAME',
                                 filename_match])
         return options        
-
-    def run_2dfdr_auto(self, dirname):
-        """Run 2dfdr in auto mode in the specified directory."""
-        # First find a file in that directory to get the date and ccd
-        for fits in self.file_list:
-            if os.path.realpath(dirname) == os.path.realpath(fits.reduced_dir):
-                date_ccd = fits.filename[:6]
-                break
-        else:
-            # No known files in that directory, best not do anything
-            return
-        with self.visit_dir(dirname):
-            # Make the 2dfdr AutoScript
-            script = ['AutoScript:SetAutoUpdate ' + date_ccd,
-                      'AutoScript:InvokeButton .auto.buttons.update',
-                      'AutoScript:InvokeButton .auto.buttons.start']
-            script_filename = '2dfdr_script.tcl'
-            f_script = open(script_filename, 'w')
-            f_script.write('\n'.join(script))
-            f_script.close()
-            # Run 2dfdr
-            idx_file = self.idx_files[date_ccd[-1]]
-            command = ['drcontrol',
-                       idx_file,
-                       '-AutoScript',
-                       '-ScriptName',
-                       script_filename]
-            print 'Running 2dfdr in', dirname
-            with open(os.devnull, 'w') as f:
-                subprocess.call(command, stdout=f)
-            # Clean up
-            os.remove(script_filename)
-        return
 
     def run_2dfdr_combine(self, file_iterable, output_path):
         """Use 2dfdr to combine the specified FITS files."""
@@ -3030,17 +2995,14 @@ class Manager:
         if isinstance(fits_or_dirname, FITSFile):
             # A FITS file has been provided, so go to its directory
             dirname = fits_or_dirname.reduced_dir
-            idx_file = self.idx_files[fits_or_dirname.ccd]
+            idx_file = self.idx_files[fits_or_dirname.grating]
         else:
             # A directory name has been provided
             dirname = fits_or_dirname
-            if 'ccd_1' in dirname:
-                idx_file = self.idx_files['ccd_1']
-            elif 'ccd_2' in dirname:
-                idx_file = self.idx_files['ccd_2']
-            else:
-                # Can't work out the CCD, let the GUI sort it out
-                idx_file = None
+            # Let the GUI sort out what idx file to use
+            # TODO: Look in the directory for a suitable fits file to work out
+            # the idx file
+            idx_file = None
         tdfdr.load_gui(dirname=dirname, idx_file=idx_file, 
                        unique_imp_scratch=True, return_to=self.cwd, 
                        restore_to=self.imp_scratch, 
