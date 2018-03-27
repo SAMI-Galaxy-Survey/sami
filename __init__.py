7323965d04395747ada4faefbbc43c77bb52cf35
Andy Green <andrew.green@aao.gov.au>
2018-03-27 16:35:57 +1100

    Fixed a bug in python 2.
    The cCovar uses ctypes, which doesn't like unicode!

--- __init__.py
+++ __init__.py
@@ -13,6 +13,7 @@ the general user, the important modules are:
 The other modules are normally not used directly, but are used by those listed
 above.
 """
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 import warnings
 
