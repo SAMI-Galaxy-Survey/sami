54c52087005ba32c5cf4c5a182e397952e71abe7
Nic Scott <exet1530@gmail.com>
2020-09-08 16:14:54 +1000

    Created hector-dev branch
    Also cleaned up old db files from repository

--- db/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Code relating to the SAMI database. This functionality was spun off as the
-sami-db code available at https://bitbucket.org/iraklis_k/samidb so this
-module can probably be removed.
-"""
-
-__all__ = ['ingest', 'export', 'query', 'utils']
