76ad7c9f1bf97e1853d06625331cf70356326318
Hector <hector@aao.gov.au>
2022-06-29 17:01:09 +1000

    remove some printings

--- dr/tdfdr.py
+++ dr/tdfdr.py
@@ -210,12 +210,8 @@ def run_2dfdr_single(fits, idx_file, options=None, dummy=False):
         raise ValueError('Unrecognised NDF_CLASS')
     out_dirname = fits.filename[:fits.filename.rindex('.')] + '_outdir'
     out_dirname_full = os.path.join('/tmp/', out_dirname) 
-    print(out_dirname_full)
     if socket.gethostname()[0:3] != 'aat':
-        print(socket.gethostname()[0:3],type(socket.gethostname()[0:3]))
         out_dirname_full = os.path.join(fits.reduced_dir, out_dirname)
-
-    print(socket.gethostname()[0:3],out_dirname_full)
 
     # Originally we directly generate os.path.join(fits.reduced_dir, out_dirname)  which however makes reduce_arc() task 60 times slower when using the machine at AAT (e.g. aatlxe). 
     # The reason is that hector home is a network mounted drive and frequent comunications by 2dfdr makes is very slow. 
